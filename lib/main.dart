264166a1969507a3990918acbf126002e273b075
OmarAmeer96 <omar.ameer244@gmail.com>
2024-07-18 17:02:41 +0300

    Changed the app icon and splash view done.

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        scaffoldBackgroundColor: Colors.white,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );

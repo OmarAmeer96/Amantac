3e4cdba415729643adbb55f33fea0f90c8936668
OmarAmeer96 <omar.ameer244@gmail.com>
2024-07-26 17:14:39 +0300

    chore: Update TODO comment in dependency_injection.dart

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -1,7 +1,7 @@
 // import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
-// TODO: If there was an error, so make it here await an make the {DioFactory} Future.
+/// TODO: If there was an error, so make it here await an make the [DioFactory] Future.
 
 final getIt = GetIt.instance;
 Future<void> setupGetIt() async {

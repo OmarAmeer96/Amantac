91ecf50a3acb5e0f7e3a527160248aaba62107b0
OmarAmeer96 <omar.ameer244@gmail.com>
2024-09-13 22:37:11 +0300

    Empty main.

--- lib/core/routing/routes.dart
+++
@@ -1,6 +0,0 @@
-class Routes {
-  static const String onBoardingView = '/onBoardingView';
-  static const String loginView = '/loginView';
-  static const String signUpScreen = '/signUpScreen';
-  static const String homeView = '/homeView';
-}
